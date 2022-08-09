b92e55c3c4e44412e260640bc9a6fd141b551d5a
yanglihz <yanglihz@cmhi.chinamobile.com>
2022-08-09 14:17:17 +0800

    fix delete namespace
    Signed-off-by: yangli <798862514@qq.com>

--- chaosblade-box-dao/src/main/java/com/alibaba/chaosblade/box/dao/model/NamespaceDO.java
+++ chaosblade-box-dao/src/main/java/com/alibaba/chaosblade/box/dao/model/NamespaceDO.java
@@ -1,6 +1,7 @@
 package com.alibaba.chaosblade.box.dao.model;
 
 import com.alibaba.chaosblade.box.dao.model.base.BaseDO;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -15,11 +16,14 @@ import lombok.experimental.FieldDefaults;
 @TableName("t_chaos_namespace")
 public class NamespaceDO extends BaseDO {
 
+    @TableField(value = "name")
     String name;
     String description;
 
+    @TableField(value = "user_id")
     String userId;
 
+    @TableField(value = "is_delete")
     Integer isDelete;
 
     String licenseKey;
